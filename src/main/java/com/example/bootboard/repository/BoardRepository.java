ffe5e66c792326c1502f57d0b2a64341cddebdad
Hojun Cha <hojun.cha997@gmail.com>
2022-09-28 23:49:41 +0900

    목록화면에 필요한 JPQL 생성

--- src/main/java/com/example/bootboard/repository/BoardRepository.java
+++ src/main/java/com/example/bootboard/repository/BoardRepository.java
@@ -4,6 +4,8 @@ package com.example.bootboard.repository;
 
 
 import com.example.bootboard.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,5 +23,15 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     //ON을 사용하여 Reply와 조인
     @Query("SELECT b, r FROM Board b LEFT OUTER JOIN Reply r ON r.board = b WHERE b.bno = :bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value = "SELECT b, w, count(r) " +
+            " FROM Board b " +
+            " LEFT JOIN b.writer w " +
+            " LEFT JOIN Reply r ON r.board = b " +
+            " GROUP BY b",
+            countQuery = "SELECT count(b) FROM Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable); // 목록 화면에 필요한 데이터
+
+
 
 }
