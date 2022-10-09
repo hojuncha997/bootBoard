f9d35e4b4de5766ca1aa74c9b03222c4dd012369
Hojun Cha <hojun.cha997@gmail.com>
2022-10-09 21:55:33 +0900

    [bootBoard] N:1(다대일) 연관관계: 10-2 화면처리: 게시물 등록 처리

--- src/main/java/com/example/bootboard/controller/BoardController.java
+++ src/main/java/com/example/bootboard/controller/BoardController.java
@@ -1,15 +1,19 @@
 //BoardController.java
 package com.example.bootboard.controller;
 
+import com.example.bootboard.dto.BoardDTO;
 import com.example.bootboard.dto.PageRequestDTO;
 import com.example.bootboard.service.BoardService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board/")
@@ -18,9 +22,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     private final BoardService boardService;
 
+    //목록
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list............." + pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
     }
+
+    //등록(get)
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+    //등록(post)
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+
+        log.info("dto..." + dto);
+    //새로 추가된 엔티티의 번호
+        Long bno = boardService.register(dto);
+
+        log.info("BNO: " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+
+
 }
