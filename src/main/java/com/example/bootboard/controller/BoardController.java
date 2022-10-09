68255cb817dcd2cc0807f18aa0f9d8d8c5d22fbb
Hojun Cha <hojun.cha997@gmail.com>
2022-10-09 22:08:22 +0900

    [bootBoard] N:1(다대일) 연관관계: 10-3 화면처리: 게시물 조회 처리

--- src/main/java/com/example/bootboard/controller/BoardController.java
+++ src/main/java/com/example/bootboard/controller/BoardController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -47,6 +44,18 @@ public class BoardController {
         redirectAttributes.addFlashAttribute("msg", bno);
 
         return "redirect:/board/list";
+    }
+
+    //조회
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO,
+                     Long bno, Model model) {
+
+        log.info("bno: " + bno);
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(boardDTO);
+        model.addAttribute("dto", boardDTO);
     }
 
 
